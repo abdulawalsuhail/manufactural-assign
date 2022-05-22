7c7ad16182b095d2d9f54ef22733bc7a579eed63
abdul awal suhail <abdullahsuhail43@gmail.com>
2022-05-22 09:09:46 +0600

    navbar added and authentication start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Navbar from './pages/Shared/Navbar';
 import Footer from './pages/Shared/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Logout from './pages/Login/Logout';
 
 function App() {
   return (
     <>
       <Navbar />
-      <></>
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/login'element={<Login/>}/>
+        <Route path='/logout'element={<Logout/>}/>
+      </Routes>
       <Footer />
     </>
   );
---
+++ src/pages/Login/Logout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logout = () => {
+    return (
+        <div>
+            <h2>Oh no I am log out</h2>
+        </div>
+    );
+};
+
+export default Logout;
