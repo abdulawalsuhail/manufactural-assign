9fe4395aaeb757d0a8e4283f23082f1ca9621685
abdul awal suhail <abdullahsuhail43@gmail.com>
2022-05-30 01:14:06 +0600

    product details updated

--- src/pages/Home/ProductDetails.js
+++ src/pages/Home/ProductDetails.js
@@ -1,158 +1,148 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
-import { toast } from 'react-toastify';
-import { useForm } from 'react-hook-form';
-import Loading from '../Shared/Loading';
-
-
-
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import auth from "../../firebase.init";
 
 const ProductDetails = () => {
-    const { productId } = useParams();
+  const { productId } = useParams();
+  const [user] = useAuthState(auth);
+  const [item, setItem] = useState({});
+//   console.log(item)
 
-    const [product, setProduct] = useState({});
-    const [user, loading, error] = useAuthState(auth);
+  // const [quantity, setQuantity] = useState(0);
 
-    const { register, formState: { errors }, getValues, watch, handleSubmit, reset } = useForm({ mode: "onBlur" });
+  useEffect(() => {
+    const url = `http://localhost:5000/product/${productId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setItem(data));
+  }, []);
 
-    useEffect(() => {
-        const url = `http://localhost:5000/product/${productId}`;
-        // console.log(url);
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setProduct(data));
+  const { register, handleSubmit, watch } = useForm();
+  const orderQuantity = watch("MOQ");
 
-    }, [])
+  const onSubmit = (data, e) => {
+    data.item = item;
+    console.log(data);
+    const url = `http://localhost:3000/order`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        e.target.reset();
+        toast.success("Your Order Success fully Added");
+      });
+  };
 
-    const onSubmit = data => {
-        // event.preventDefault();
-        // const perproduct = event.target.perproduct.value;
-
-        if (data.MOQ > product?.instock) {
-
-            toast.error('Sorry!!You can not order maximum order quantity')
-
-
-            return;
-        }
-        const totalPrice = parseInt(data?.MOQ) * parseInt(product.perproduct);
-        const booking = {
-            product: product.name,
-            productMOQ: data.MOQ,
-            customer: user.email,
-            customerName: user.displayName,
-            phone: data.phone,
-            totalPrice: totalPrice
-        }
-        console.log(booking)
-
-        fetch('http://localhost:5000/order', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(booking)
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (!data?.success) {
-                    toast(`Your order done.`)
-                    reset()
-                }
-                // else {
-                //     toast.error(`Already have an order.`)
-                // }
-                // setProduct();
-
-            });
-    }
-
-    if (loading) {
-        <Loading />
-    }
-    return (
-        <div>
-            <div class="hero min-h-screen ">
-                <div class="hero-content flex-col lg:flex-row">
-                    <img src={product?.img} alt="" class="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 class="text-4xl font-bold my-8"> {product?.name}</h1>
-                        <p><span class="text-accent font-bold">Product description</span>: {product?.description}</p>
-                        <h2> <span class="text-accent font-bold">Product Price</span> : ${product?.perproduct}/pp</h2>
-                        <h2> <span class="text-accent font-bold">Product In Stock</span> : {product?.instock}</h2>
-                        <h2><span class="text-accent font-bold">Minimum Order Quantity</span> : {product?.minimumorder}</h2>
-                    </div>
+  return (
+    <div className="">
+      <div className="card purchase-container lg:card-side bg-base-100">
+        <figure>
+          <img className="w-96 p-5" src={item.img} alt="Album" />
+        </figure>
+        <div className="card-body p-5">
+          <h2 className="card-title">{item.name}</h2>
+          <p>{item.description}</p>
+          <div className="product-detail">
+            <p className="text-3xl">
+              {" "}
+              Price : <span className="font-bold">{item.perproduct}$</span>
+            </p>
+            <p className="text-xl">
+              <small>
+                Stock :{" "}
+                <span className="text-orange-500 font-bold">
+                  {item.minimumorder}p
+                </span>
+              </small>
+            </p>
+            <p className="text-xl">
+              <small>
+                Minimum Order :{" "}
+                <span className="text-orange-500 font-bold">
+                  {" "}
+                  {item.minOrder}p
+                </span>
+              </small>
+            </p>
+          </div>
+          <div className="">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className=" order-detail mt-5">
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    value={user?.displayName}
+                    readOnly
+                    {...register("name", { required: true })}
+                    className="input"
+                  />
                 </div>
-            </div>
-            <div class="py-4">
-                <h1 class="text-3xl font-bold my-8 text-center">Purchase Now : {product?.name}</h1>
-                <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
-                    <div class="form-control w-full max-w-xs">
-                        <label class="label">
-                            <span class="label-text">Your name</span>
-                        </label>
-                        <input type="text" name="name" placeholder='Your Name' className="input input-bordered w-full max-w-xs"   {...register("name", {
-                            required: {
-                                value: true,
-                                message: 'Name is Required'
-                            }
-                        })} />
-                        <label className="label">
-                            {errors?.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors?.name.message}</span>}
-                        </label>
-                    </div>
 
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    value={user?.email}
+                    readOnly
+                    {...register("email", { required: true })}
+                    className="input"
+                  />
+                </div>
 
-                    <div class="form-control w-full max-w-xs">
-                        <label class="label">
-                            <span class="label-text">Your email</span>
-                        </label>
-                        <input type="email" name="email" placeholder='Your email' className="input input-bordered w-full max-w-xs"   {...register("email", {
-                            required: {
-                                value: true,
-                                message: 'email is Required'
-                            }
-                        })} />
-                        <label className="label">
-                            {errors?.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors?.email.message}</span>}
-                        </label>
-                    </div>
+                <div>
+                  <input
+                    type="number"
+                    placeholder="Phone"
+                    {...register("phone", { required: true })}
+                    className="input"
+                  />
+                </div>
 
-                    <div class="form-control w-full max-w-xs">
-                        <label class="label">
-                            <span class="label-text">Your Address</span>
-                        </label>
-                        <input type="text" name="address" placeholder='Your Address' className="input input-bordered w-full max-w-xs"   {...register("address", {
-                            required: true
-                        })} />
-                        <label className="label">
-                            {errors?.address?.type === 'required' && <span className="label-text-alt text-red-500">{errors?.address.message}</span>}
-                        </label>
-                    </div>
-
-                    <div class="form-control w-full max-w-xs">
-                        <label class="label">
-                            <span class="label-text">Your phone</span>
-                        </label>
-                        <input type="number"  placeholder='Your phone' className="input input-bordered w-full max-w-xs"   {...register("phone", {
-                            required: true
-                        })} />
-                        <label className="label">
-                            {errors?.phone?.type === 'required' && <span className="label-text-alt text-red-500">{errors?.phone.message}</span>}
-                            {errors?.phone?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors?.phone.message}</span>}
-                        </label>
-                    </div>
-
-                    <input type="number"  placeholder="Minimum order Quantity 100 " className="input input-bordered w-full max-w-xs"  {...register("MOQ")} />
-
-
-                    <input type="submit" value="Purchase" className="btn btn-accent w-full max-w-xs" />
-                </form>
-            </div>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Your Address"
+                    {...register("address", { required: true })}
+                    className="input"
+                  />
+                </div>
+                <div>
+                  <input
+                    type="number"
+                    placeholder="Order Quantity"
+                    {...register("MOQ", { required: true })}
+                    className="input"
+                  />
+                </div>
+                <div className="">
+                  <button
+                    disabled={
+                      orderQuantity < item.minimumorder ||
+                      orderQuantity > item.instock
+                    }
+                    type="submit"
+                    className="btn btn-accent"
+                  >
+                    Order Now
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
