675b964f94382fa750ec37f79faf0494c2c7001b
abdul awal suhail <abdullahsuhail43@gmail.com>
2022-05-29 08:46:39 +0600

    fetch data mypurchase

--- src/pages/Dashboard/MyPurchase.js
+++ src/pages/Dashboard/MyPurchase.js
@@ -6,30 +6,31 @@ import auth from '../../firebase.init';
 
 const MyPurchase = () => {
     const [purchases, setPurchases] = useState([]);
+    console.log(purchases)
     const [user] = useAuthState(auth);
     const navigate = useNavigate()
 
 
     useEffect(() => {
-        if (user) {
-            fetch(`http://localhost:5000/product?customer=${user.email}`, {
-                method: 'GET',
-                headers: {
-                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+
+        fetch(`http://localhost:5000/order`, {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                // console.log('res', res);
+                if (res.status === 401 || res.status === 403) {
+                    signOut(auth);
+                    localStorage.removeItem('accessToken');
+                    navigate('/');
                 }
+                return res.json()
             })
-                .then(res => {
-                    // console.log('res', res);
-                    if (res.status === 401 || res.status === 403) {
-                        signOut(auth);
-                        localStorage.removeItem('accessToken');
-                        navigate('/');
-                    }
-                    return res.json()
-                })
-                .then(data => setPurchases(data));
-        }
-    }, [user])
+            .then(data => setPurchases(data));
+
+    }, [user, navigate])
 
     return (
         <div>
@@ -41,30 +42,28 @@ const MyPurchase = () => {
                             <th></th>
                             <th>Name</th>
                             <th>Price</th>
-                            <th>In Stock</th>
                             <th>Payment</th>
                             <th>Order Quantity</th>
-                            <th>Order Details</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                        purchases.map((a, index) => <tr key={a._id}>
-                            <th>{index + 1}</th>
-                            <td>{a.customerName}</td>
-                            <td>{a.product}</td>
-                            <td>{a.instock}</td>
-                            <td>{a.price}</td>
-                            {/* <td>{a.price}</td> */}
-                            <td>
-                                {(a.price && !a.paid) && <Link to={`/dashboard/payment/${a._id}`}><button className='btn btn-xs btn-success'>pay</button></Link>}
-                                {(a.price && a.paid) && <div>
-                                    <p><span className='text-success'>Paid</span></p>
-                                    <p>Transaction id: <span className='text-success'>{a.transactionId}</span></p>
-                                </div>}
-                            </td>
-                        </tr>)
-                    }
+                            purchases.map((a, index) => <tr key={a._id}>
+                                <th>{index + 1}</th>
+                                <td>{a.customerName}</td>
+                                <td>{a.totalPrice}</td>
+                                <td>{}</td>
+                                <td>{a.MOQ}</td>
+                                {/* <td>{a.price}</td> */}
+                                <td>
+                                    {(a.price && !a.paid) && <Link to={`/dashboard/payment/${a._id}`}><button className='btn btn-xs btn-success'>pay</button></Link>}
+                                    {(a.price && a.paid) && <div>
+                                        <p><span className='text-success'>Paid</span></p>
+                                        <p>Transaction id: <span className='text-success'>{a.transactionId}</span></p>
+                                    </div>}
+                                </td>
+                            </tr>)
+                        }
 
 
                     </tbody>
